23809a95e5577c1f90159aaadc416dbf048c4b1f
Dima Berry <dimaberry1@gmail.com>
2024-06-13 00:28:41 +0200

    solve methods of number 14

---
+++ 8/Training JS #14 Methods of Number object--toString() and toLocaleString().js
@@ -0,0 +1,5 @@
+function colorOf(r, g, b) {
+  return `#${r.toString(16).length === 1 ? `0${r.toString(16)}` : r.toString(16)}${
+    g.toString(16).length === 1 ? `0${g.toString(16)}` : g.toString(16)
+  }${b.toString(16).length === 1 ? `0${b.toString(16)}` : b.toString(16)}`;
+}
